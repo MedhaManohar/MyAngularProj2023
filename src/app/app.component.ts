3a3685203a51d2222d10bda293ac459f7e643ece
MedhaManohar27 <medhakale@gmail.com>
2023-06-11 19:48:32 +0530

    First Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'RestaurentApp';
+export class AppComponent 
+{
+  title = 'MarvellousRestaurentApp';
 }
